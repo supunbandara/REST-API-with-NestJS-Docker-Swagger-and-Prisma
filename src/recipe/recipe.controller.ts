1ef7951078ed5db68a6912ac476dc5ed9a85b833
SupunBandara <mwsupun01@gmail.com>
2024-02-03 22:51:11 +0530

    initial commit

---
+++ src/recipe/recipe.controller.ts
@@ -0,0 +1,44 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { RecipeService } from './recipe.service';
+import { CreateRecipeDto } from './dto/create-recipe.dto';
+import { UpdateRecipeDto } from './dto/update-recipe.dto';
+
+@Controller('recipe')
+export class RecipeController {
+  constructor(private readonly recipeService: RecipeService) {}
+
+  @Post()
+  create(@Body() createRecipeDto: CreateRecipeDto) {
+    return this.recipeService.create(createRecipeDto);
+  }
+
+  @Get()
+  async findAll() {
+    const data = await this.recipeService.findAll();
+    console.log(data);
+    return data;
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.recipeService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateRecipeDto: UpdateRecipeDto) {
+    return this.recipeService.update(+id, updateRecipeDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.recipeService.remove(+id);
+  }
+}
